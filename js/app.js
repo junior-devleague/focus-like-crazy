4959e66ea57e4453471cb688fe329fbf73f67025
Yoscroll <ianhenry.c808@gmail.com>
2017-04-10 08:53:14 -1000

    checking master

--- js/app.js
+++ js/app.js
@@ -4,6 +4,14 @@ function bubbleClick(id){
   console.log(id+" was clicked");
   document.getElementById(id).style.transition = "all 0.3s";
   document.getElementById(id).style.opacity = "0";
+  document.getElementById(id).removeEventListener('bubbleClick',
+        makeBackgroundYellow,
+        false
+    );
+  setInterval(wait, 300);
+  function wait() {
+    document.getElementById(id).style.display = "none";
+  }
 }
 
 function blueButtonClick(id){
