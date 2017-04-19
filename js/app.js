d42cdf168bfde951ce3f54c0a90fc448b42d718d
Yoscroll <ianhenry.c808@gmail.com>
2017-04-19 09:17:22 -1000

    updating branches_ian

--- js/app.js
+++ js/app.js
@@ -14,7 +14,21 @@ function bubbleClick(id){
       document.getElementById(id).style.opacity = "0";
       console.log(id+" was clicked");
 }
-
 function blueButtonClick(id){
 	var button = document.getElementById(id);
 }
+
+//timer and pause screen
+/*
+if "start" button clicked
+  then start timer
+if "pause" button clicked
+  then pause timer
+  then transition to pause screen
+*/
+
+//end game
+/*
+if timer reaches 1 minute || all bubbles are clicked
+  then transition to end game screen
+*/
